e51df0e81a47dc3bb1b28c6f1623fdb9405023fe
Elad Ben-Israel <elad.benisrael@gmail.com>
2011-07-29 15:22:01 -0700

    add jsonp support

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ app.get(/.*/, function(req, res) {
     var lat = req.query.lat;
     var lon = req.query.lon;
     if (!lat || !lon) {
-        res.send('<p>usage: /?lat=LATITUDE&lon=LONGITUDE[&limit=LIMIT]</p><p>eg: <a href="/?lat=47.5492515563965&lon=-122.251714706421&limit=200">/?lat=47.5492515563965&lon=-122.251714706421&limit=200</a></p>', 400);
+        res.send('<p>usage: GET /<br/><li>lat=LATITUDE</li><li>lon=LONGITUDE</li><li><i>optional: </i>limit=LIMIT</li><li><i>optional: </i>callback=JSONP_CALLBACK</li></p><p>eg: <a href="/?lat=47.5492515563965&lon=-122.251714706421&limit=200">/?lat=47.5492515563965&lon=-122.251714706421&limit=200&callback=callme</a></p>', 400);
         return;
     }
     
@@ -52,6 +52,11 @@ app.get(/.*/, function(req, res) {
                   image: doc.sources[0].url
             });
         });
+        
+        var jsonp = req.query.callback;
+        if (jsonp) {
+          result = jsonp + ' (' + JSON.stringify(result) + ')';
+        }
         
         res.send(result);
     }; 
