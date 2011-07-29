93869f92a25121c4e55da8b48ff70d28dee4efeb
Elad Ben-Israel <elad.benisrael@gmail.com>
2011-07-29 14:27:01 -0700

    updates

--- index.js
+++ index.js
@@ -1,7 +1,15 @@
 var app = require('express').createServer();
 
 app.get(/.*/, function(req, res) {
-  res.send('<p>hello world</p>');
+  var lat = req.query.lat;
+  var lon = req.query.lon;
+  if (!lat || !lon) {
+    res.send('please specify lat & lon in the query parameters', 400);
+    return;
+  }
+  
+  console.log('lat', lat, 'lon', lon);
+  res.send('<p>response with lat: '+lat+','+lon+'</p>');
 });
 
 app.listen(process.argv[3]);
